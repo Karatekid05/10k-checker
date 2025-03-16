dd255d0a447b1c5420230440680a2b95d2f87fd9
karatekid <dgrua50@proton.me>
2025-03-16 19:21:04 +0000

    Remove unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ChakraProvider, Box, VStack, Input, Button, Text, Heading, useToast, Flex, Center } from '@chakra-ui/react'
+import { ChakraProvider, Box, VStack, Input, Button, Text, Heading, useToast } from '@chakra-ui/react'
 import AdminPage from './AdminPage'
 import whitelist from './whitelist.json'
 
